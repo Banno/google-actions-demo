ea073b1eae9ff3dbedac55284e30c85f38cfe160
Chad Killingsworth <ckillingsworth@jackhenry.com>
2021-01-22 13:28:33 -0600

    Handle only one account and prompt to ask for additional accounts

--- webhooks/actions_on_google_fulfillment/index.js
+++ webhooks/actions_on_google_fulfillment/index.js
@@ -65,6 +65,10 @@ function sortOrderFromAccountType(accountType) {
   }
 }
 
+app.handle('create_user', (conv) => {
+  logData('create_user', conv.user);
+});
+
 const transparentImage = new Image({
   url: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
   alt: '',
@@ -73,7 +77,7 @@ const transparentImage = new Image({
 });
 app.handle('load_accounts', async (conv) => {
   if (!conv.user || !conv.user.params.bearerToken) {
-    conv.scene.next = 'account_linking';
+    conv.scene.next.name = 'account_linking';
     return;
   }
 
@@ -87,17 +91,19 @@ app.handle('load_accounts', async (conv) => {
 
   if (!conv.session.params.accounts) {
     logData(`Fetching data for user ${conv.session.params.userInfo.sub}`);
-    const fetchResponse = await fetch(`https://${bannoOnlineHostname}/a/consumer/api/v0/users/${conv.session.params.userInfo.sub}/fetch`, {
-      method: 'PUT',
-      headers: {
-        authorization: `Bearer ${conv.user.params.bearerToken}`
-      }
-    }).then(ensureResponseValid.bind(null, 'fetch response'));
+    const fetchResponse = await fetch(
+        `https://${bannoOnlineHostname}/a/consumer/api/v0/users/${conv.session.params.userInfo.sub}/fetch`, {
+          method: 'PUT',
+          headers: {
+            authorization: `Bearer ${conv.user.params.bearerToken}`
+          }
+        }).then(ensureResponseValid.bind(null, 'fetch response'));
 
     /** @type {!Array<{type: string}>} */ const initialEvents = [];
     for (
         let taskPayload = {events: initialEvents, version: 0};
-        taskPayload.events.find((evt) => evt.type === 'TaskEnded' || evt.type === 'EnrichmentAllAccountBalancesUpdated') === undefined;
+        taskPayload.events.find(
+            (evt) => evt.type === 'TaskEnded' || evt.type === 'EnrichmentAllAccountBalancesUpdated') === undefined;
     ) {
       taskPayload = await fetch(
           `https://${bannoOnlineHostname}/a/consumer/api/users/${conv.session.params.userInfo.sub}/tasks/${fetchResponse.taskId}?sinceVersion=${taskPayload.version}`,
@@ -154,44 +160,41 @@ app.handle('load_accounts', async (conv) => {
         }));
     if (validAccounts === 0) {
       conv.add(`I was not able to find any accounts.`);
-      conv.scene.next = 'actions.scene.END_CONVERSATION';
+      conv.scene.next.name = 'actions.scene.END_CONVERSATION';
       return;
     }
     conv.session.params.accounts = validAccounts;
   }
+});
 
-  const typeDataEntries = [];
-  const listItems = [];
-  conv.session.params.accounts.forEach((acct, index) => {
-    let accountId = `ACCOUNT_${acct.id.replace(/-/g, '_')}`;
-    typeDataEntries.push({
-      name: accountId,
-      synonyms: [accountId, acct.name],
-      display: {
-        title: acct.name,
-        description: `${acct.type} Account`,
-        image: transparentImage
+app.handle('select_account', async (conv) => {
+  if (conv.session.params.accounts.length > 1) {
+    const typeDataEntries = [];
+    const listItems = [];
+    conv.session.params.accounts.forEach((acct, index) => {
+      let accountId = `ACCOUNT_${acct.id.replace(/-/g, '_')}`;
+      typeDataEntries.push({
+        name: accountId,
+        synonyms: [accountId, acct.name],
+        display: {
+          title: acct.name,
+          description: `${acct.type} Account`,
+          image: transparentImage
+        }
+      });
+      listItems.push({key: accountId});
+    });
+
+    // Override prompt_option Type with display
+    conv.session.typeOverrides = conv.session.typeOverrides || [];
+    conv.session.typeOverrides.push({
+      name: 'account_name',
+      mode: 'TYPE_REPLACE',
+      synonym: {
+        entries: typeDataEntries
       }
     });
-    listItems.push({key: accountId});
-  });
 
-  // Override prompt_option Type with display
-  conv.session.typeOverrides = conv.session.typeOverrides || [];
-  conv.session.typeOverrides.push({
-    name: 'account_name',
-    mode: 'TYPE_REPLACE',
-    synonym: {
-      entries: typeDataEntries
-    }
-  });
-  if (conv.session.params.accounts.length === 1) {
-    conv.scene.slots.account_name = {
-      mode: 'REQUIRED',
-      status: 'FILLED',
-      value: conv.session.params.accounts[0].name
-    };
-  } else {
     conv.add(`Looks like you have ${conv.session.params.accounts.length} accounts. Which one are you interested in?`);
     conv.add(new List({
       title: `${conv.session.params.userInfo.name}'s Accounts`,
@@ -201,22 +204,25 @@ app.handle('load_accounts', async (conv) => {
 });
 
 app.handle('current_balance', async (conv) => {
-  const accountId = conv.session.params.account_name.substr('account_'.length).replace(/_/g, '-');
-  const account = conv.session.params.accounts.find((acct) => acct.id === accountId);
-
-  if (account) {
-    const balanceLabel = account.availableBalace !== undefined ? 'an available' : 'a current';
-    const balanceValue = account.availableBalace !== undefined ? account.availableBalace : account.balance;
-    const formattedBalance = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-        .format(balanceValue);
-
-    conv.add(`Your ${account.name} account has ${balanceLabel} balance of ${formattedBalance}.`);
-  } else {
-    conv.add('Oops something went wrong. Please try again.');
+  let account = conv.session.params.accounts[0];
+  if (conv.session.params.account_name) {
+    const accountId = conv.session.params.account_name.substr('account_'.length).replace(/_/g, '-');
+    account = conv.session.params.accounts.find((acct) => acct.id === accountId);
   }
 
   if (conv.session.params.accounts.length === 1) {
-    conv.scene.next = 'actions.scene.END_CONVERSATION';
+    conv.scene.next.name = 'actions.scene.END_CONVERSATION';
+  }
+
+  if (account) {
+    const balanceLabel = account.availableBalance !== undefined ? 'an available' : 'a current';
+    const balanceValue = account.availableBalance !== undefined ? account.availableBalance : account.balance;
+    const formattedBalance = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+        .format(balanceValue);
+
+    return conv.add(`Your ${account.name} account has ${balanceLabel} balance of ${formattedBalance}.`);
+  } else {
+    return conv.add('Oops something went wrong. Please try again.');
   }
 });
 
